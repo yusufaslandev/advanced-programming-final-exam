aacb142ea53b644cd1257ca0c54add07be594195
yusufasln26 <yusufasln26@users.noreply.github.com>
2018-06-04 03:56:16 +0300

    advanced final project

---
+++ advancedfinalexam/src/model/Manufacturer.java
@@ -0,0 +1,121 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the manufacturer database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Manufacturer.findAll", query="SELECT m FROM Manufacturer m")
+public class Manufacturer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="manufacturer_id")
+	private Integer manufacturerId;
+
+	private String addressline1;
+
+	private String addressline2;
+
+	private String city;
+
+	private String email;
+
+	private String fax;
+
+	private String name;
+
+	private String phone;
+
+	private String state;
+
+	private Integer zip;
+
+	public Manufacturer() {
+	}
+
+	public Integer getManufacturerId() {
+		return this.manufacturerId;
+	}
+
+	public void setManufacturerId(Integer manufacturerId) {
+		this.manufacturerId = manufacturerId;
+	}
+
+	public String getAddressline1() {
+		return this.addressline1;
+	}
+
+	public void setAddressline1(String addressline1) {
+		this.addressline1 = addressline1;
+	}
+
+	public String getAddressline2() {
+		return this.addressline2;
+	}
+
+	public void setAddressline2(String addressline2) {
+		this.addressline2 = addressline2;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFax() {
+		return this.fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return this.phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getState() {
+		return this.state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Integer getZip() {
+		return this.zip;
+	}
+
+	public void setZip(Integer zip) {
+		this.zip = zip;
+	}
+
+}
