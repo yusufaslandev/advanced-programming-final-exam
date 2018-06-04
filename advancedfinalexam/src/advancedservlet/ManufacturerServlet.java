36463d9edb18ad2d9523f0e5bda0e82e4a41d5f8
yusufasln26 <yusufasln26@users.noreply.github.com>
2018-06-04 04:00:26 +0300

    advanced final
    final project for exam

---
+++ advancedfinalexam/src/advancedservlet/ManufacturerServlet.java
@@ -0,0 +1,76 @@
+package advancedservlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Manufacturer;
+import model.Product;
+
+/**
+ * Servlet implementation class ManufacturerServlet
+ */
+@WebServlet("/ManufacturerServlet")
+public class ManufacturerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ManufacturerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		EntityManagerFactory emf= (EntityManagerFactory)getServletContext().getAttribute("emf");
+		EntityManager em = emf.createEntityManager();
+		 
+		     
+		     try 
+		     {
+		    	  Manufacturer manu=new Manufacturer();
+		          em.getTransaction().begin();
+		          em.persist(manu);
+		      
+		          
+		          List<Manufacturer> ManufacturerList = em.createQuery(
+		                  "SELECT g FROM manufacturer g", Manufacturer.class).getResultList();
+		              request.setAttribute("ManufacturerList", ManufacturerList);
+		              request.getRequestDispatcher("/index.jsp")
+		                  .forward(request, response);
+		     } 
+		     catch (Exception e) 
+		     {
+		        
+		     }
+		     finally {
+		    	 if (em.getTransaction().isActive())
+		                em.getTransaction().rollback();
+		            em.close();
+		     }
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
